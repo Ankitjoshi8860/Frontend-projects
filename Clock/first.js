f1060320b0206abb1b20acebda5648c1780db60e
Ankit Joshi <ankitjoshi8860@gmail.com>
2025-08-30 01:31:19 +0530

    clock mini project

---
+++ Clock/first.js
@@ -0,0 +1,22 @@
+// timer Project
+
+function timing()
+{
+const timer = document.getElementById("root")
+const now=new Date();
+const indian_time=now.toLocaleTimeString();
+timer.innerHTML=indian_time;
+
+}
+
+setInterval(timing,1000);
+
+const timer = document.getElementById("root")
+
+timer.style.fontSize = "200px"
+timer.style.display = "flex";
+timer.style.height="100vh"
+timer.style.justifyContent = "center"
+timer.style.alignItems = "center"
+
+
